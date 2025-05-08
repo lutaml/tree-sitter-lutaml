531cd2b8cd1c335f293c21d32e35d012e30ae867
Jeffrey Lau <jeffrey.lau@ribose.com>
2025-05-08 16:51:51 +0800

    fix: Fix rust binding test

--- bindings/rust/lib.rs
+++ bindings/rust/lib.rs
@@ -5,12 +5,15 @@
 //!
 //! ```
 //! let code = r#"
+//! diagram MyDiagram {
+//!   title "My Title"
+//! }
 //! "#;
 //! let mut parser = tree_sitter::Parser::new();
 //! let language = tree_sitter_lutaml::LANGUAGE;
 //! parser
 //!     .set_language(&language.into())
-//!     .expect("Error loading Lutaml parser");
+//!     .expect("Error loading LutaML parser");
 //! let tree = parser.parse(code, None).unwrap();
 //! assert!(!tree.root_node().has_error());
 //! ```
@@ -36,7 +39,7 @@ pub const NODE_TYPES: &str = include_str!("../../src/node-types.json");
 
 // NOTE: uncomment these to include any queries that this grammar contains:
 
-// pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
+pub const HIGHLIGHTS_QUERY: &str = include_str!("../../queries/highlights.scm");
 // pub const INJECTIONS_QUERY: &str = include_str!("../../queries/injections.scm");
 // pub const LOCALS_QUERY: &str = include_str!("../../queries/locals.scm");
 // pub const TAGS_QUERY: &str = include_str!("../../queries/tags.scm");
